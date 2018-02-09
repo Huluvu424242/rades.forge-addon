ba6657cb5e27d1c673e00dfe041e44aa8c268c48
FunThomas424242 <funthomas424242@gmail.com>
2018-02-09 19:04:48 +0100

    bintray upload support added to pom.xml
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/project/commands/UpdateMavenCommand.java
+++ src/main/java/com/github/funthomas424242/rades/project/commands/UpdateMavenCommand.java
@@ -5,6 +5,8 @@ import com.github.funthomas424242.flowdesign.Integration;
 import com.github.funthomas424242.rades.core.resources.UIResourceHelper;
 import com.github.funthomas424242.rades.project.RadesProject;
 import com.github.funthomas424242.rades.project.RadesProjectBuilder;
+import org.apache.maven.model.DeploymentRepository;
+import org.apache.maven.model.DistributionManagement;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
@@ -19,12 +21,13 @@ import org.jboss.forge.addon.ui.util.Categories;
 import org.jboss.forge.addon.ui.util.Metadata;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class UpdateMavenCommand extends AbstractProjectUICommand {
 
-    public static final String COMMAND_NAME = "rades-project-update";
+    public static final String COMMAND_NAME = "rades-pom-update";
 
     @Inject
     protected UIResourceHelper commandHelper;
@@ -33,7 +36,7 @@ public class UpdateMavenCommand extends AbstractProjectUICommand {
     public UICommandMetadata getMetadata(UIContext context) {
         return Metadata.forCommand(UpdateMavenCommand.class)
                 .name(COMMAND_NAME)
-                .description("Update assets of a RADES project.")
+                .description("Update pom.xml of a RADES project.")
                 .category(Categories.create(CATEGORY_RADES_PROJECT));
     }
 
@@ -61,30 +64,58 @@ public class UpdateMavenCommand extends AbstractProjectUICommand {
         if (!pomXML.exists() || pomXML.getContents(StandardCharsets.UTF_8).isEmpty()) {
             // create new pom.xml
             final Model pomModel = new Model();
-            pomModel.setModelVersion("4.0.0");
-            pomModel.setGroupId(radesProject.getGroupID());
-            pomModel.setArtifactId(radesProject.getArtifactID());
-            pomModel.setVersion(radesProject.getVersion());
-            pomModel.setDescription(radesProject.getProjectDescription());
-            final MavenXpp3Writer writer = new MavenXpp3Writer();
-            final OutputStream ostream = pomXML.getResourceOutputStream();
-            writer.write(ostream, pomModel);
+            copyRadesProjectToPomModel(radesProject, pomModel);
+            writeModelToPomXml(pomModel, pomXML);
         } else {
             // Update existing pom.xml
             final boolean shouldOverride = prompt.promptBoolean("Soll die aktuelle pom.xml ersetzt werden?", false);
             if (shouldOverride) {
                 final MavenXpp3Reader pomReader = new MavenXpp3Reader();
                 final Model pomModel = pomReader.read(pomXML.getResourceInputStream());
-                pomModel.setGroupId(radesProject.getGroupID());
-                pomModel.setArtifactId(radesProject.getArtifactID());
-                pomModel.setVersion(radesProject.getVersion());
-                pomModel.setDescription(radesProject.getProjectDescription());
-                final MavenXpp3Writer writer = new MavenXpp3Writer();
-                final OutputStream ostream = pomXML.getResourceOutputStream();
-                writer.write(ostream, pomModel);
+                copyRadesProjectToPomModel(radesProject, pomModel);
+                writeModelToPomXml(pomModel, pomXML);
             }
         }
         return Results
                 .success("Kommando '" + COMMAND_NAME + "' wurde erfolgreich ausgeführt.");
+    }
+
+    protected void writeModelToPomXml(final Model pomModel, final FileResource pomXML) throws IOException {
+        final MavenXpp3Writer writer = new MavenXpp3Writer();
+        final OutputStream ostream = pomXML.getResourceOutputStream();
+        writer.write(ostream, pomModel);
+    }
+
+    protected void copyRadesProjectToPomModel(final RadesProject radesProject, final Model pomModel) {
+        pomModel.setModelVersion("4.0.0");
+        pomModel.setModelEncoding("UTF-8");
+
+        // projekt maven coordinaten + beschreibung
+        pomModel.setGroupId(radesProject.getGroupID());
+        pomModel.setArtifactId(radesProject.getArtifactID());
+        pomModel.setVersion(radesProject.getVersion());
+        pomModel.setDescription(radesProject.getProjectDescription());
+
+        //
+        if (hasFullBintraySupportInfo(radesProject)) {
+            final DistributionManagement distributionManagement = new DistributionManagement();
+            final DeploymentRepository deploymentRepository = new DeploymentRepository();
+            deploymentRepository.setId("bintray-" + radesProject.getBintrayUsername() + "-" + radesProject.getBintrayRepositoryname());
+            deploymentRepository.setName(radesProject.getBintrayUsername() + "-" + radesProject.getBintrayRepositoryname());
+            deploymentRepository.setUrl("https://api.bintray.com/maven/"
+                    + radesProject.getBintrayUsername() + "/"
+                    + radesProject.getBintrayRepositoryname() + "/"
+                    + radesProject.getBintrayPackagename() + "/;publish=1"
+            );
+            distributionManagement.setRepository(deploymentRepository);
+            pomModel.setDistributionManagement(distributionManagement);
+        }
+    }
+
+    protected boolean hasFullBintraySupportInfo(final RadesProject radesProject) {
+        return (radesProject.getBintrayUsername() != null)
+                && (radesProject.getBintrayRepositoryname() != null)
+                && (radesProject.getBintrayPackagename() != null);
+
     }
 }
